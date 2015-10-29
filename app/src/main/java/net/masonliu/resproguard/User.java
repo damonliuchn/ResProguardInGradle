06f0b77cfef72a9783ed5aec49581a0e7da4cc1b
MasonLiu <masonliuchn@gmail.com>
2015-10-29 13:48:43 +0800

    first commit

---
+++ app/src/main/java/net/masonliu/resproguard/User.java
@@ -0,0 +1,13 @@
+package net.masonliu.resproguard;
+
+/**
+ * Created by liumeng on 10/16/15.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
